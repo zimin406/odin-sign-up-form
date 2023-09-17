5f975267c23921f7719f5dba311203c7b03f79a5
Gwak Jimin <zimin406@yonsei.ac.kr>
2023-09-17 22:17:33 +0900

    Add password checking functionality

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const passwordInput = document.querySelector("input#user-password");
+const passwordConfirmInput = document.querySelector("input#user-password-confirm");
+const errorMessage = document.querySelector("div.user-password-confirm.form-container>span");
+
+passwordConfirmInput.addEventListener("focus", (event) => {
+    errorMessage.textContent = "";
+});
+
+passwordConfirmInput.addEventListener("blur", (event) => {
+    const password = passwordInput.value;
+    const passwordConfirm = passwordConfirmInput.value;
+    if ((password.length > 0) && (password !== passwordConfirm)) {
+        errorMessage.textContent = "* Passwords do not match";
+    }
+});
+
